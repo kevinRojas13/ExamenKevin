89da79f99aebe713f510be0064151bbc48220f1e
kevinRojas13 <202000700b@utea.edu.pe>
2023-09-22 07:33:15 -0500

    agregando color, boton calcular

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,8 @@ class _InputPageState extends State<InputPage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Calculadora de IMC'),
@@ -42,9 +44,22 @@ class _InputPageState extends State<InputPage> {
               ),
             ),
           ),
-          Text(
-            'Peso (kg): ${weight.toStringAsFixed(1)}',
-            style: TextStyle(fontSize: 18),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Peso:',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 10),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  '${weight.toStringAsFixed(1)} KG',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
           ),
           Slider(
             value: weight,
@@ -55,11 +70,25 @@ class _InputPageState extends State<InputPage> {
                 weight = newValue;
               });
             },
+            activeColor: Colors.red,
           ),
           SizedBox(height: 20),
-          Text(
-            'Altura (cm): ${height.toStringAsFixed(1)}',
-            style: TextStyle(fontSize: 18),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Altura:',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 10),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  '${height.toStringAsFixed(1)} CM',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
           ),
           Slider(
             value: height,
@@ -70,6 +99,33 @@ class _InputPageState extends State<InputPage> {
                 height = newValue;
               });
             },
+            activeColor: Colors.red,
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Container(
+              width: screenWidth /
+                  2, // Establece el ancho a la mitad de la pantalla
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción a realizar cuando se presiona el botón "Calcular"
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey, // Cambia el color de fondo a gris
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.calculate), // Icono para la insignia
+                    SizedBox(width: 5),
+                    Text(
+                      'Calcular',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
